7500ca409672d3b07f05a631e29aa0b0e4f42e39
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2005-12-10 11:51:46 +0100

    missing file - ColourHighlight.hs
    darcs-hash:20051210105146-6871e-c96c7e097bea34ffe2a45bee488f20255de815e7

---
+++ ColourHighlight.hs
@@ -0,0 +1,20 @@
+module ColourHighlight
+  ( Colour(..)
+  , Highlight(..)
+  ) where
+
+data Colour = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
+  deriving (Eq,Show,Read,Enum)
+
+data Highlight =
+    Normal
+  | Bold
+  | Dim
+  | Underscore
+  | Blink
+  | ReverseVideo
+  | Concealed
+  | Foreground Colour
+  | Background Colour
+  deriving (Eq,Show,Read)
+
