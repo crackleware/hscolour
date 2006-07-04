67401e7061a6c828af451ad4716500acc7a45fe3
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2006-07-04 12:46:19 +0200

    minor tidy-ups of the main program, mainly to introduce a -version option
    darcs-hash:20060704104619-6871e-0c055d7a12deb302f830ec14434d3c1d1e49d717

--- HsColour.hs
+++ HsColour.hs
@@ -4,31 +4,37 @@ import Language.Haskell.HsColour
 import Language.Haskell.HsColour.Colourise (readColourPrefs)
 
 import System
-import IO (hFlush,stdout)
+import IO (hPutStrLn,hFlush,stdout,stderr)
+
+version = "1.4"
 
 main :: IO ()
 main = do
   prog <- System.getProgName
   args <- System.getArgs
   pref <- readColourPrefs
-  let (ioWrapper,output,anchors) = case args of
-        []                -> help prog
-        ["-h"]            -> help prog
-        ["-help"]         -> help prog
-        ["-tty"]          -> (Prelude.interact, TTY,  False)
-        ["-html"]         -> (Prelude.interact, HTML, False)
-        ["-css"]          -> (Prelude.interact, CSS,  False)
-        ["-anchorHTML"]   -> (Prelude.interact, HTML, True)
-        ["-anchorCSS"]    -> (Prelude.interact, CSS,  True)
-        [a]               -> (fileInteract a, TTY,  False)
-        ["-tty",a]        -> (fileInteract a, TTY,  False)
-        ["-html",a]       -> (fileInteract a, HTML, False)
-        ["-css",a]        -> (fileInteract a, CSS,  False)
-        ["-anchorHTML",a] -> (fileInteract a, HTML, True)
-        ["-anchorCSS",a]  -> (fileInteract a, CSS,  True)
-        _           -> help prog
+  (ioWrapper,output,anchors) <- case args of
+        []                -> errorOut (help prog)
+        ["-h"]            -> errorOut (help prog)
+        ["-help"]         -> errorOut (help prog)
+        ["-v"]            -> errorOut (prog++" "++version)
+        ["-version"]      -> errorOut (prog++" "++version)
+        ["--version"]     -> errorOut (prog++" "++version)
+        ["-tty"]          -> return (Prelude.interact, TTY,  False)
+        ["-html"]         -> return (Prelude.interact, HTML, False)
+        ["-css"]          -> return (Prelude.interact, CSS,  False)
+        ["-anchorHTML"]   -> return (Prelude.interact, HTML, True)
+        ["-anchorCSS"]    -> return (Prelude.interact, CSS,  True)
+        [a]               -> return (fileInteract a, TTY,  False)
+        ["-tty",a]        -> return (fileInteract a, TTY,  False)
+        ["-html",a]       -> return (fileInteract a, HTML, False)
+        ["-css",a]        -> return (fileInteract a, CSS,  False)
+        ["-anchorHTML",a] -> return (fileInteract a, HTML, True)
+        ["-anchorCSS",a]  -> return (fileInteract a, CSS,  True)
+        _                 -> errorOut (help prog)
   ioWrapper (hscolour output pref anchors)
   hFlush stdout
   where
     fileInteract f u = do readFile f >>= putStr . u
-    help p = error ("Usage: "++p++" [-tty|-html|-css|-anchor|-anchorCSS] [file.hs]")
+    errorOut s = hPutStrLn stderr s >> hFlush stderr >> exitFailure
+    help p = "Usage: "++p++" [-tty|-html|-css|-anchor|-anchorCSS] [file.hs]"
