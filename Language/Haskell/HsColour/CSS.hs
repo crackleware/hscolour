971dfdc7d36ae125a25925ab8a6f082ccafdf488
bjorn <bjorn@bringert.net>
2006-07-04 06:51:48 +0200

    Added CSS.hscolourFragment.
    darcs-hash:20060704045148-6cdb2-03766246bad4dbf8ed635bb336f845381ec2db4b

--- Language/Haskell/HsColour/CSS.hs
+++ Language/Haskell/HsColour/CSS.hs
@@ -1,21 +1,33 @@
 -- | Formats Haskell source code as HTML with CSS.
-module Language.Haskell.HsColour.CSS (hscolour) where
+module Language.Haskell.HsColour.CSS (hscolour, hscolourFragment) where
 
 import Language.Haskell.HsColour.Anchors
 import Language.Haskell.HsColour.Classify as Classify
 import Language.Haskell.HsColour.HTML (renderAnchors, escape)
 
--- | Formats Haskell source code as HTML with CSS.
+-- | Formats Haskell source code as a complete HTML document with CSS.
 hscolour :: Bool   -- ^ Whether to include anchors
          -> String -- ^ Haskell source code.
-         -> String -- ^ Coloured Haskell source code.
-hscolour anchor = 
-    top'n'tail . (if anchor 
-                  then concatMap (renderAnchors renderToken) . insertAnchors
-                  else concatMap renderToken) . tokenise
+         -> String -- ^ An HTML document containing the coloured 
+                   --   Haskell source code.
+hscolour anchor = top'n'tail . hscolourFragment anchor
+
+-- | Formats Haskell source code as an HTML fragment with CSS.
+--   No stylesheet link is included in the output.
+hscolourFragment :: Bool   -- ^ Whether to include anchors
+                 -> String -- ^ Haskell source code.
+                 -> String -- ^ An HTML fragment containing the coloured 
+                           --   Haskell source code.
+hscolourFragment anchor = 
+    pre . (if anchor 
+           then concatMap (renderAnchors renderToken) . insertAnchors
+           else concatMap renderToken) . tokenise
 
 top'n'tail :: String -> String
 top'n'tail  = (cssPrefix++) . (++cssSuffix)
+
+pre :: String -> String
+pre = ("<pre>"++) . (++"</pre>")
 
 renderToken :: (TokenType,String) -> String
 renderToken (Space,text) = text
@@ -35,5 +47,5 @@ cssClass Char     = "chr"
 cssClass Number   = "num"
 cssClass Error    = "sel"
 
-cssPrefix = "<html><head><link type='text/css' rel='stylesheet' href='hscolour.css'/></head><body><pre>"
-cssSuffix = "</pre></body></html>"
+cssPrefix = "<html><head><link type='text/css' rel='stylesheet' href='hscolour.css'/></head><body>"
+cssSuffix = "</body></html>"
