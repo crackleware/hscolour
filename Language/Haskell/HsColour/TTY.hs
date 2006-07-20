3c5dad8d17e5d4d64c0c12d0491013d9875d2c97
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2006-07-20 16:57:00 +0200

    qualify a name, just for clarity
    darcs-hash:20060720145700-6871e-0ac6151eddb1b9a3ac736d7e97dce82ed9ca579d

--- Language/Haskell/HsColour/TTY.hs
+++ Language/Haskell/HsColour/TTY.hs
@@ -1,7 +1,7 @@
 -- | Highlights Haskell code with ANSI terminal codes.
 module Language.Haskell.HsColour.TTY (hscolour) where
 
-import Language.Haskell.HsColour.ANSI
+import Language.Haskell.HsColour.ANSI as ANSI
 import Language.Haskell.HsColour.Classify
 import Language.Haskell.HsColour.Colourise
 
@@ -12,4 +12,4 @@ hscolour :: ColourPrefs -- ^ Colour preferences.
 hscolour pref = concatMap (renderToken pref) . tokenise
 
 renderToken :: ColourPrefs -> (TokenType,String) -> String
-renderToken pref (t,s) = highlight (colourise pref t) s
+renderToken pref (t,s) = ANSI.highlight (colourise pref t) s
