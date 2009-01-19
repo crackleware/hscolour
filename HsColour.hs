9b0722c40d01f6e5cbe2ee192b94683193cee333
Malcolm.Wallace <Malcolm.Wallace@cs.york.ac.uk>
2009-01-19 14:25:55 +0100

    Update version to 1.11
    darcs-hash:20090119132555-6871e-2a302ebafd4bea3f045006de7b58d787d17e0a05

--- HsColour.hs
+++ HsColour.hs
@@ -10,7 +10,7 @@ import Monad (when)
 import List  (intersperse)
 import Debug.Trace
 
-version = "1.10.1"
+version = "1.11"
 
 optionTable :: [(String,Option)]
 optionTable = [ ("help",    Help)
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 LIBRARY	= hscolour
-VERSION	= 1.10.1
+VERSION	= 1.11
 
 DIRS	= Language/Haskell/HsColour
 
